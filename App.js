4c0f34127c3967ace9116a4c5262a75920d49150
JosueNieves0702 <123047834@upq.edu.mx>
2026-06-09 13:49:14 -0600

    Practica6

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
+/*zona de importaciones*/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Saludo } from './components/Saludo';
+import {Saludo2} from './components/Saludo2';
+import Perfil from './components/Perfil';
+/*zona main*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hola mundo react native</Text>
+      <Text>--------------------------------</Text>
+      <Saludo></Saludo>
+      <Saludo />
+      <Text>----------------------------------------</Text>
+      <Saludo2></Saludo2>
+      <Image source={require('./assets/wave.png')} />
       <StatusBar style="auto" />
+      <Perfil></Perfil>
     </View>
   );
 }
-
+/* estilos y posisionamiento*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ components/Perfil.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Perfil = () => {
+  return (
+    <View>
+      <Text>Cristian Hurtado Lucas</Text>
+      <Text>Ingeniería en Sistemas Computacionales</Text>
+      <Text>Programación móvil</Text>
+      <Text>S-204</Text>
+    </View>
+  );
+};
+
+export default Perfil;
