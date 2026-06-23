fb554d60db3fafa62de4d528c4eaffbdb02706e8
JosueNieves0702 <123047834@upq.edu.mx>
2026-06-22 22:25:14 -0600

    practica 11

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import React,{useState} from 'react'
 import TargetasScreen from './TargetasScreen';
 import SaveAreaScreen from './SafeAreaScreen';
+import Pre_sable_and_switch from './Pre_sable_and_switch';
 
 /*zona main*/
 export default function MenuScreen() {
@@ -14,6 +15,8 @@ export default function MenuScreen() {
       return <TargetasScreen/>;
     case 'safearea':
       return <SaveAreaScreen/>;
+    case 'presable':
+      return <Pre_sable_and_switch/>;
     case 'menu':
     default:
   
@@ -21,7 +24,7 @@ export default function MenuScreen() {
     <View style={styles.container}>
         <Button title='practica targetas' onPress={()=>setScreen('targetas')}/>
         <Button title='practica SaveArea' onPress={()=>setScreen('safearea')}/>
-        <Button title= 'practica targetas'/>
+        <Button title='practica presable y switch' onPress={()=>setScreen('presable')}/>
         <StatusBar style="auto" />
      
     </View>
